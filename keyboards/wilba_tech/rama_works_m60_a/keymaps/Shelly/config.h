db135e12e6b1ed3e463856f3153abe6a745c7ef1
Sky Gao <skygao@apple.com>
2020-03-09 11:18:41 -0700

    Adding all the profiles

---
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/Shelly/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// place overrides here
+#define RGB_BACKLIGHT_ENABLED 0
---
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/Shelly/keymap.c
@@ -0,0 +1,52 @@
+#include QMK_KEYBOARD_H
+
+#define DFT 0
+#define FNC 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* 
+Layer 0: Default Layer - Mac
++-----------------------------------------------------------+
+|Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
+|-----------------------------------------------------------|
+|Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
+|-----------------------------------------------------------|
+|Cap   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|   Enter|
+|-----------------------------------------------------------|
+|Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| Shift|Gui|
++-----------------------------------------------------------+
+      |Ctl| Alt |           Space           | Alt | Fn|      
+      +-----------------------------------------------+
+*/
+
+[DFT] = LAYOUT_60_hhkb( \
+ KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL,KC_BSLS,KC_GRV, \
+ KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSPC,        \
+KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT, KC_ENT,                \
+KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,KC_RGUI,                \
+        KC_LCTL,KC_LALT,                 KC_SPC,                        KC_RALT,MO(FNC)),
+
+/* 
+Layer 2: Function Layer
++-----------------------------------------------------------+
+|Ejc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|F13|F14|
+|-----------------------------------------------------------|
+|     |Hom| U |End|PgU|   |   | % | ^ | & | * |VoD|VoU| Mute|
+|-----------------------------------------------------------|
+|      | L | D | R |PgD|   |   | ~ | + | - | * | / |        |
+|-----------------------------------------------------------|
+|        |   |   |   |   |   |   |   |Prv|Nxt|P/P|      |   |
++-----------------------------------------------------------+
+      |   |     |                       |     |   |
+      +-------------------------------------------+
+*/
+
+[FNC]= LAYOUT_60_hhkb( \
+KC_EJCT,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, KC_F13,KC_F14, \
+KC_TRNS,KC_HOME,  KC_UP, KC_END,KC_PGUP,KC_TRNS,KC_TRNS,KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_VOLD,KC_VOLU,KC_MUTE,        \
+KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_TRNS,KC_TRNS,KC_TILD,KC_PPLS,KC_PMNS,KC_PAST,KC_PSLS,KC_TRNS,                \
+KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MPRV,KC_MNXT,KC_MPLY,KC_TRNS,KC_TRNS,                \
+        KC_TRNS,KC_TRNS,                KC_TRNS,                        KC_TRNS,KC_TRNS),
+
+};
